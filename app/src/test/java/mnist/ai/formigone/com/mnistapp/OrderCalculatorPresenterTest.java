d5ea38544c3fbbdf555e23376cd40e57b42b004f
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-09-19 13:18:09 -0600

    Use Mockito

--- app/src/test/java/mnist/ai/formigone/com/mnistapp/OrderCalculatorPresenterTest.java
+++ app/src/test/java/mnist/ai/formigone/com/mnistapp/OrderCalculatorPresenterTest.java
@@ -1,64 +1,53 @@
 package mnist.ai.formigone.com.mnistapp;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 import mnist.ai.formigone.com.mnistapp.presenters.OrderCalculatorPresenter;
 import mnist.ai.formigone.com.mnistapp.repositories.OrderCalculatorRepository;
 import mnist.ai.formigone.com.mnistapp.views.OrderCalculatorView;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class OrderCalculatorPresenterTest {
+    @Mock
+    OrderCalculatorRepository mockRepository;
+
+    @Mock
+    OrderCalculatorView mockView;
+
+    private OrderCalculatorPresenter presenter;
+    private int totalRenders;
+
+    @Before
+    public void setUp() {
+        presenter = new OrderCalculatorPresenter(mockView, mockRepository);
+        totalRenders = 0;
+    }
+
     @Test
-    public void changingQuantityCausesRerender() throws Exception {
-        OrderCalculatorRepository mockRepository = new MockOrderCalculatorRepository();
-        OrderCalculatorView mockView = new MockOrderCalculatorView();
-        OrderCalculatorPresenter presenter = new OrderCalculatorPresenter(mockView, mockRepository);
+    public void changingQuantityCausesRerender() {
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) {
+                totalRenders += 1;
+                return null;
+            }
+        }).when(mockView).displayPrice(Mockito.anyDouble());
 
-        Assert.assertEquals(0, ((MockOrderCalculatorView)mockView).renderCount);
+        Assert.assertEquals(0, totalRenders);
         presenter.changeQuantity(0);
-        Assert.assertTrue(((MockOrderCalculatorView)mockView).renderCount > 0);
-    }
-
-    private class MockOrderCalculatorRepository implements OrderCalculatorRepository {
-        @Override
-        public int changeQuantity(int quantity) {
-            return 0;
-        }
-
-        @Override
-        public int getQuantity() {
-            return 0;
-        }
-
-        @Override
-        public double getPrice() {
-            return 0;
-        }
-    }
-
-    private class MockOrderCalculatorView implements OrderCalculatorView {
-        int renderCount = 0;
-
-        @Override
-        public void displayQuantity(int quantity) {
-            renderCount += 1;
-        }
-
-        @Override
-        public void displayPrice(double price) {
-            renderCount += 1;
-        }
-
-        @Override
-        public void setMinusButtonClickable(boolean clickable) {
-            renderCount += 1;
-        }
+        Assert.assertTrue(totalRenders > 0);
     }
 }
