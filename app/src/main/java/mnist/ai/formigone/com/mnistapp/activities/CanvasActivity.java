0a46a8322b85d15bac1452ac7209b71ddaedbedf
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-09-21 07:02:32 -0600

    Basic canvas drawing

--- app/src/main/java/mnist/ai/formigone/com/mnistapp/activities/CanvasActivity.java
+++ app/src/main/java/mnist/ai/formigone/com/mnistapp/activities/CanvasActivity.java
@@ -1,15 +1,23 @@
 package mnist.ai.formigone.com.mnistapp.activities;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 
 import mnist.ai.formigone.com.mnistapp.R;
+import mnist.ai.formigone.com.mnistapp.views.CanvasView;
 
 public class CanvasActivity extends AppCompatActivity {
+    private static final String TAG = "CanvasActivity";
+    private CanvasView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,25 @@ public class CanvasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_canvas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        surfaceView = (CanvasView)findViewById(R.id.surfaceView);
+        surfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int i = event.getActionIndex();
+                Log.v(TAG, "Touched " + event.getX(i) + ", " + event.getY(i) + " => " + i + ", " + event.getAction());
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        surfaceView.addPointGroup();
+                        surfaceView.addPoint(new Point((int)event.getX(i), (int)event.getY(i)));
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        surfaceView.addPoint(new Point((int)event.getX(i), (int)event.getY(i)));
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
 }
